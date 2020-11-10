8db01733efa7b9cfca1e2efab020a2efe0ea6b9b
Corey Carter <crc2231@gmail.com>
2020-11-10 15:00:02 -0700

    Create user structure specs

--- spec/requests/api/v1/new_user_request_spec.rb
+++ spec/requests/api/v1/new_user_request_spec.rb
@@ -4,7 +4,7 @@ describe 'API' do
   it 'sends New User Request' do
 
     user_params = {
-      "email": "whatever@example.com",
+      "email": "whatever@aol.com",
       "password": "password",
       "password_confirmation": "password"
     }
@@ -13,5 +13,17 @@ describe 'API' do
     post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
 
     expect(response).to be_successful
+    user = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(user).to have_key(:id)
+    expect(user).to have_key(:type)
+    expect(user[:type]).to eq("users")
+    expect(user).to have_key(:attributes)
+    expect(user[:attributes]).to have_key(:email)
+    expect(user[:attributes][:email]).to eq(user_params[:email])
+    expect(user[:attributes]).to have_key(:api_key)
+    expect(user[:attributes]).to_not have_key(:password)
+    expect(user[:attributes]).to_not have_key(:password_digest)
+
   end
 end
