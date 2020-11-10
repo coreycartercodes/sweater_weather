a9cf2cbdf20bdb4088f1e5b82302905c87886c51
Corey Carter <crc2231@gmail.com>
2020-11-10 15:00:46 -0700

    Remove attr reader from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
 require 'securerandom'
 class User < ApplicationRecord
-  before_create :generate_api_key
-  has_secure_password
-  before_save 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
-  attr_reader :email, :id, :api_key
-
-
-  private
-  def generate_api_key
-    self.api_key = SecureRandom.urlsafe_base64
-  end
+  validates :api_key, uniqueness: true, presence: true
+  has_secure_password
 
 end
