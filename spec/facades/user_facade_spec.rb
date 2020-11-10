fbc31d2f9215f26132cfdf9c042ad0a7912d6430
Corey Carter <crc2231@gmail.com>
2020-11-10 16:35:41 -0700

    Add sad path specs

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe UserFacade do
-  it "returns user PORO" do
+  it "returns user PORO from create" do
     user_params = {
       "email": "whatever@aol.com",
       "password": "password",
@@ -13,4 +13,15 @@ describe UserFacade do
     expect(user.email).to eq(user_params[:email])
     expect(user.api_key).to be_a(String)
   end
-end
+
+  it "returns errors for create" do
+    user_params = {
+      "email": "",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    
+    user = UserFacade.create(user_params)
+    expect(user.errors.full_messages.to_sentence).to eq("Email can't be blank")
+  end
+  end
