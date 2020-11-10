cd672ff06b448fac2beb2a54ccddacd20d591b2b
Corey Carter <crc2231@gmail.com>
2020-11-10 15:01:42 -0700

    Add api_key to user model

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_163425) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "api_key"
     t.string "password_digest"
