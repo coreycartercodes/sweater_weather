d17e99cf46aa93fd1860fffc4e82a95785ae2026
Corey Carter <crc2231@gmail.com>
2020-11-10 15:00:23 -0700

    Add Create method to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,21 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    binding.pry
-    user_params = JSON.parse(request.body.read, symbolize_names: true, :quirks_mode => true)
-    user = User.new(user_params)
+    user_params = JSON.parse(request.env['RAW_POST_DATA'], symbolize_names: true)
+    user = UserFacade.create(user_params)
     if user.save
-      # session[:current_user] = user.id
       render json: UserSerializer.new(user), status: 201
     else
       render json: {errors: user.errors.full_messages.to_sentence}, status: 400
     end
   end
 
-  def find
-    render json: UserSerializer.new(User.find_by(params[:email]))
-  end
+  # def find
+  #   render json: UserSerializer.new(User.find_by(params[:email]))
+  # end
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :api_key, :password, :password_confirmation)
   end
 end
