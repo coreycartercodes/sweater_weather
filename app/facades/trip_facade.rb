0aa6097d5a533bf299847456fea9f507af99d1f6
Corey Carter <crc2231@gmail.com>
2020-11-10 21:02:30 -0700

    Add trip_info action

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,8 +1,15 @@
 class TripFacade
 
   def self.trip_info(origin, destination)
-    location = LocationService.directions(origin, destination)
-binding.pry
-    weather = WeatherService
+    directions = LocationService.directions(origin, destination)
+    dest_coords = LocationService.get_location(destination)
+    weather = WeatherService.forecast(dest_coords[:lat], dest_coords[:lng])
+    trip_data = {
+                  start_city: origin,
+                  end_city: destination,
+                  location_data: directions,
+                  weather_data: weather
+                }
+    Trip.new(trip_data)
   end
 end
